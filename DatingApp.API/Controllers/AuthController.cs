6bc420b54ae728918f46cb75774b06a5438d541e
Fraser Carpenter <fraser@hqtech.co.za>
2019-09-19 16:57:45 +0200

    Second Commit

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(string username, string password){
+            
+        }
+    }
+}
