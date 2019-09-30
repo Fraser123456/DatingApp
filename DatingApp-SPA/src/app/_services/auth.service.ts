93fa06ae884a609e94147169017b1d77d6fdc3ad
Fraser Carpenter <fraser@hqtech.co.za>
2019-09-30 11:10:12 +0200

    end of section 4

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
 
 constructor(private http: HttpClient) { }
 
-login(model: any){
+login(model: any) {
   return this.http.post(this.baseUrl + 'login', model)
   .pipe(
     map((response: any) => {
@@ -19,6 +19,10 @@ login(model: any){
       }
     })
   );
+}
+
+register(model:any){
+  return this.http.post(this.baseUrl + 'register', model);
 }
 
 }
